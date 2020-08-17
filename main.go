07f585169ce865e43892636b9271960a84f8b702
Humam Fauzi <humam.fauzi.93@gmail.com>
2020-08-17 15:48:11 +0700

    add middleware for logging

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	log.Println("Init server")
 	router := mux.NewRouter()
+	router.Use(LoggerMiddleware)
 	router.HandleFunc("/", HomeHandler).Methods(http.MethodGet)
 
 	server := &http.Server{
@@ -20,12 +21,19 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	log.Fatal(server.ListenAndServe())
 	log.Println("Server working")
+	log.Fatal(server.ListenAndServe())
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "HomePage")
 	return
+}
+
+func LoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("ACCESSED", r.Header.Get("User-Agent"))
+		next.ServeHTTP(w, r)
+	})
 }
