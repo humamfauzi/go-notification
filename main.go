1e435c8dc3cf5c6c9f2dfde708207d8631201148
Humam Fauzi <humam.fauzi.93@gmail.com>
2020-08-17 15:34:38 +0700

    add server to test in browser

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Init server")
+	log.Println("Init server")
 	router := mux.NewRouter()
 	router.HandleFunc("/", HomeHandler).Methods(http.MethodGet)
+
+	server := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(server.ListenAndServe())
+	log.Println("Server working")
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
