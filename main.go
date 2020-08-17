57fa0930da95994eab59231db899152d2d9cfdfa
Humam Fauzi <humam.fauzi.93@gmail.com>
2020-08-17 16:15:30 +0700

    add pseudo login handler

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -14,7 +16,7 @@ func main() {
 	router := mux.NewRouter()
 	router.Use(LoggerMiddleware)
 	router.HandleFunc("/", HomeHandler).Methods(http.MethodGet)
-
+	router.HandleFunc("/users/login", LoginHandler).Methods(http.MethodPost)
 	server := &http.Server{
 		Handler:      router,
 		Addr:         "127.0.0.1:8000",
@@ -36,4 +38,45 @@ func LoggerMiddleware(next http.Handler) http.Handler {
 		log.Println("ACCESSED", r.Header.Get("User-Agent"))
 		next.ServeHTTP(w, r)
 	})
+}
+
+type UserLogin struct {
+	Username string
+	Password string
+	Token    *string
+}
+
+func (ul UserLogin) AuthenticateLogin() bool {
+	return true
+}
+
+func (ul *UserLogin) GenerateToken() {
+	newToken := "newToken"
+	ul.Token = &newToken
+}
+
+type LoginReply struct {
+	Success bool   `json:"success"`
+	Token   string `json:"token"`
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	var loginProfile UserLogin
+	json.Unmarshal(body, &loginProfile)
+
+	if ok := loginProfile.AuthenticateLogin(); !ok {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	loginProfile.GenerateToken()
+
+	loginReply := LoginReply{
+		Success: true,
+		Token:   *loginProfile.Token,
+	}
+
+	jsonReply, _ := json.Marshal(loginReply)
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonReply)
 }
