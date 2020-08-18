d697bd98c6ef08b344f82b00dbc7bb3c48e4c843
Humam Fauzi <humam.fauzi.93@gmail.com>
2020-08-18 14:24:43 +0700

    add in memory for storage

--- main.go
+++ main.go
@@ -2,16 +2,80 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
+type Storage interface {
+	Get(string) (UserLogin, error)
+}
+
+type Environement string
+
+func (env Environement) getCurrentEnv() string {
+	return os.Getenv("GO_ENV")
+}
+func (env Environement) IsTest() bool {
+	return env.getCurrentEnv() == "test"
+}
+
+type InMemory struct {
+	Members []UserLogin
+}
+
+func (im InMemory) Get(userName string) (UserLogin, error) {
+	for _, user := range im.Members {
+		if user.Username == userName {
+			return user, nil
+		}
+	}
+	return UserLogin{}, errors.New("USER_NOT_FOUND")
+}
+
+func CreateInMemoryStorage() Storage {
+	token1 := "HEHE123"
+	token2 := "1253NNMK"
+	newStorage := InMemory{
+		Members: []UserLogin{
+			UserLogin{
+				Username: "Hello123",
+				Password: "AXZ098",
+				Token:    &token1,
+			},
+			UserLogin{
+				Username: "Bye456",
+				Password: "IOP678",
+				Token:    &token2,
+			},
+		},
+	}
+	return newStorage
+}
+func initStorage() (Storage, error) {
+	var env Environement
+	if env.IsTest() {
+		return CreateInMemoryStorage(), nil
+	} else {
+		return InMemory{}, errors.New("unsupported env")
+	}
+}
+
+var storage Storage
+var err error
+
 func main() {
+	storage, err = initStorage()
+	if err != nil {
+		panic(err)
+	}
 	log.Println("Init server")
 	router := mux.NewRouter()
 	router.Use(LoggerMiddleware)
@@ -41,18 +105,30 @@ func LoggerMiddleware(next http.Handler) http.Handler {
 }
 
 type UserLogin struct {
-	Username string
-	Password string
+	Username string `json:"username"`
+	Password string `json:"password"`
 	Token    *string
 }
 
+func (ul *UserLogin) InterpretPayload(payload io.Reader) {
+	body, _ := ioutil.ReadAll(payload)
+	json.Unmarshal(body, ul)
+}
+
 func (ul UserLogin) AuthenticateLogin() bool {
+	_, err := storage.Get(ul.Username)
+	if err != nil {
+		return false
+	}
 	return true
 }
 
 func (ul *UserLogin) GenerateToken() {
-	newToken := "newToken"
-	ul.Token = &newToken
+	user, err := storage.Get(ul.Username)
+	if err != nil {
+		ul.Token = nil
+	}
+	ul.Token = user.Token
 }
 
 type LoginReply struct {
@@ -61,14 +137,13 @@ type LoginReply struct {
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body)
-	var loginProfile UserLogin
-	json.Unmarshal(body, &loginProfile)
-
+	loginProfile := UserLogin{}
+	loginProfile.InterpretPayload(r.Body)
 	if ok := loginProfile.AuthenticateLogin(); !ok {
 		w.WriteHeader(http.StatusForbidden)
 		return
 	}
+
 	loginProfile.GenerateToken()
 
 	loginReply := LoginReply{
