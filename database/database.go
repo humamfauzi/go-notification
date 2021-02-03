a5cccd19800d5aa1c9106a2eb377307092d84138
Humam Fauzi <humam.fauzi.93@gmail.com>
2021-02-03 15:59:55 +0700

    add database configuration

--- database/database.go
+++ database/database.go
@@ -3,13 +3,30 @@ package database
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
-	// "fmt"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"strings"
+	"strconv"
+	"errors"
+	"context"
 )
 
 var (
 	db *sql.DB
 	queryMap QueryMap
 )
+
+type QueryMap map[string]map[string]interface{}
+
+func (qm QueryMap) GetQuery(path string) (string, error) {
+	stringSplit := strings.Split(path, ".")
+	result, ok := qm[stringSplit[0]][stringSplit[1]].(string)
+	if !ok {
+		return "", errors.New("QUERY MAP PATH NOT FOUND")
+	}
+	return result, nil
+}
 
 type MysqlDatabaseAccess struct {
 	Username string
@@ -19,9 +36,7 @@ type MysqlDatabaseAccess struct {
 	DBName string
 }
 
-type QueryMap map[string]string
-
-func ConnectDatabase(mda MysqlDatabaseAccess) (*sql.DB, error) {
+func (mda MysqlDatabaseAccess) ConnectDatabase() (*sql.DB, error) {
 	composed := mda.Username + ":"
 	composed += mda.Password + "@"
 	composed += mda.Protocol + "("
@@ -34,50 +49,364 @@ func ConnectDatabase(mda MysqlDatabaseAccess) (*sql.DB, error) {
 	return db, nil
 }
 
+type ITransaction interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+}
+
+func CreateTransaction(dbFunction func(tx *sql.Tx) error) error {
+	ctx := context.Background()
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	if err := dbFunction(tx); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func ConvertJsonToQueryMap(dir string) error {
+	body, err := ioutil.ReadFile(dir)
+	if err != nil {
+		return err
+	}
+	qm := make(QueryMap)
+	err = json.Unmarshal(body, &qm)
+	if err != nil {
+		return err
+	}
+	queryMap = qm
+	return nil
+}
+
+func WriteToDB(tx ITransaction, path string, input ...interface{}) error {
+	query, err := Query(path, input...)
+	if err != nil {
+		return err
+	}
+	fmt.Println(query)
+	write, err := tx.Query(query)
+	if err != nil {
+		return err
+	}
+	write.Close()
+	return nil
+}
+
+func Query(path string, input ...interface{}) (string, error) {
+	formatQuery, err := queryMap.GetQuery(path)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf(formatQuery, input...), nil
+}
+
+func createColumnValuePairing(pairs [][]string) string {
+	if len(pairs) == 0 {
+		return ""
+	}
+	finalQuery := " WHERE "
+	for i := 0; i < len(pairs); i++ {
+		if len(pairs[i]) == 1 {
+			finalQuery += fmt.Sprintf(" %s ", pairs[i])
+		} else if len(pairs[i]) == 3 {
+			finalQuery += fmt.Sprintf(" %s %s '%s' ", pairs[i][0], pairs[i][1], pairs[i][2])
+		}
+	}
+	return finalQuery
+}
+
+func ReadFromDB(tx ITransaction, path string, selectColumn []string, whereColumn [][]string) (RowsScan, error) {
+	column := strings.Join(selectColumn, ",")
+	whereQuery := createColumnValuePairing(whereColumn)
+	query, err := Query(path, column, whereQuery)
+	fmt.Println(query)
+	if err != nil {
+		return &sql.Rows{}, err
+	}
+	rows, err := tx.Query(query)
+	if err != nil {
+		return &sql.Rows{}, err
+	}
+	return rows, nil
+}
+
 func CloseConnection() {
 	db.Close()
 }
 
+// ------- USER MODEL FUNCTION --------- //
 type UserProfile struct {
 	Email string
 	Id string
 }
 
-func GetUser(userId string) (UserProfile, error) {
-	var userProfile UserProfile
-	row := db.QueryRow("SELECT id, email FROM users WHERE id=?", userId)
-	switch err := row.Scan(&userProfile.Id, &userProfile.Email); err {
-	case nil:
-		return userProfile, nil
-	default:
-		return userProfile, err
-	}
+type RowsScan interface {
+	Next() bool
+	Scan(...interface{}) error
+	Close() error
+	Err() error
 }
 
-func InsertUserEmailAndId(userProfile UserProfile) bool {
-	insert, err := db.Query("INSERT INTO users (id, email) VALUES (?, ?)", userProfile.Id, userProfile.Email)
+func (up *UserProfile) Get(tx ITransaction) error {
+	path := "users.get"
+	selectColumn := []string{"email", "id"}
+	wherePairs := [][]string{
+		[]string{
+			"id", "=", up.Id,
+		},
+	}
+	rows, err := ReadFromDB(tx, path, selectColumn, wherePairs)
 	if err != nil {
-		return false
+		return err
 	}
-	insert.Close()
-	return true
+	if err := up.Scan(rows); err != nil {
+		return err
+	}
+	return nil
 }
 
-func UpdateUserEmail(userProfile UserProfile) bool {
-	update, err := db.Query("UPDATE users SET email = ? WHERE id = ?", userProfile.Email, userProfile.Id)
+func (up *UserProfile) Scan(rows RowsScan) error {
+	defer rows.Close()
+	count := 0
+	for rows.Next(){	
+		if err := rows.Scan(&up.Email, &up.Id); err != nil {
+			return err
+		}
+		count++
+	}
+	if count == 0 {
+		return sql.ErrNoRows
+	}
+	return nil
+}
+
+func (up UserProfile) InsertFormat() string {
+	return fmt.Sprintf("('%s','%s')", up.Id, up.Email)
+}
+
+func (up UserProfile) Insert(tx ITransaction) error {
+	path := "users.create"
+	if err := WriteToDB(tx, path, up.InsertFormat()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (up UserProfile) UpdateFormat() string {
+	return fmt.Sprintf("'%s'", up.Email)
+}
+
+func (up UserProfile) Update(tx ITransaction) error {
+	path := "users.update"
+	if err := WriteToDB(tx, path, up.UpdateFormat(), up.Id); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (up UserProfile) DeleteFormat() string {
+	return fmt.Sprintf("'%s'", up.Id)
+}
+
+func (up UserProfile) Delete(tx ITransaction) error {
+	path := "users.delete"
+	if err := WriteToDB(tx, path, up.DeleteFormat()); err != nil {
+		return err
+	}
+	return nil
+}
+
+type UserProfiles []UserProfile
+
+func (up UserProfiles) BulkCreate(tx ITransaction) error {
+	for i:=0; i < len(up); i++ {
+		err := up[i].Insert(tx)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (up UserProfiles) BulkDelete(tx ITransaction) error {
+	for i:=0; i < len(up); i++ {
+		err := up[i].Delete(tx)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ------- TOPIC MODEL FUNCTION --------- //
+type Topic struct {
+	Id int
+	UserId string
+	Title string
+	Desc string
+}
+
+func (t Topic) InsertFormat() string {
+	return fmt.Sprintf("('%s','%s','%s')", t.UserId, t.Title, t.Desc)
+}
+
+func (t Topic) Insert(tx ITransaction) error {
+	path := "topic.insert"
+	if err := WriteToDB(tx, path, t.InsertFormat()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t Topic) UpdateFormat() string {
+	return fmt.Sprintf("(%s,%s)", t.Title, t.Desc)
+}
+
+func (t Topic) Update(tx ITransaction) error {
+	path := "topic.update"
+	if err := WriteToDB(tx, path, t.UpdateFormat); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t Topic) Delete(tx ITransaction) error {
+	path := "topic.delete"
+	if err := WriteToDB(tx, path, t.Id); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ------- SUBSCRIBER MODEL FUNCTION --------- //
+type Subscriber struct {
+	Id int
+	TopicId int
+	UserId string
+}
+func (s Subscriber) CreateFormat() string {
+	return fmt.Sprintf("(%d,%s)", s.TopicId, s.UserId)
+}
+
+func (s Subscriber) Create(tx ITransaction) error {
+	path:= "subscriber.create"
+	if err := WriteToDB(tx, path, s.CreateFormat()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Subscriber) DeleteFormat() string {
+	return fmt.Sprintf("%d", s.Id)
+}
+
+func (s Subscriber) Delete(tx ITransaction) error {
+	path := "subscriber.delete"
+	if err := WriteToDB(tx, path, s.DeleteFormat()); err != nil {
+		return err
+	}
+	return nil
+}
+
+// -------- NOTIFICATION MODEL FUNCTION --------- //
+type Notification struct {
+	Id int
+	UserId string
+	TopicId int
+	Message string
+	IsRead bool
+}
+
+func (n Notification) InsertFormat() string {
+	return fmt.Sprintf("('%s',%d,'%s')", n.UserId, n.TopicId, n.Message)
+}
+
+func (n Notification) Insert(tx ITransaction) error {
+	path := "notification.insertNotification"
+	if err := WriteToDB(tx, path, n.InsertFormat()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (n *Notification) Get(tx ITransaction) error {
+	path := "notification.get"
+	selectColumn := []string{"id", "user_id", "topic_id", "message", "is_read"}
+	wherePairs := [][]string{
+		[]string{
+			"id", "=", fmt.Sprintf("%d", n.Id),
+		},
+	}
+	rows, err := ReadFromDB(tx, path, selectColumn, wherePairs)
 	if err != nil {
-		return false
+		return err
 	}
-	update.Close()
-	return true
+	if err := n.Scan(rows); err != nil {
+		return err
+	}
+	return nil
 }
 
-func DeleteUser(userProfile UserProfile) bool {
-	delete, err := db.Query("DELETE FROM users WHERE id = ?", userProfile.Id) 
-	if err != nil {
-		return false
+func (n *Notification) Scan(rows RowsScan) error {
+	defer rows.Close()
+	count := 0
+	for rows.Next() {
+		if err := rows.Scan(&n.Id, &n.UserId, &n.TopicId, &n.Message, &n.IsRead); err != nil {
+			return err
+		}
+		count++
 	}
-	delete.Close()
-	return true
+	if count == 0 {
+		return sql.ErrNoRows
+	}
+	return nil
 }
 
+type Notifications []Notification
+
+func (n Notifications) InsertFormat() string {
+	finalQuery := make([]string, len(n))
+	for i:=0; i < len(n); i++ {
+		finalQuery[i] = n[i].InsertFormat()
+	}
+	return strings.Join(finalQuery, ",")
+}
+
+func (n Notifications) Insert(tx ITransaction) error {
+	path := "notification.bulkInsertNotification"
+	if err := WriteToDB(tx, path, n.InsertFormat()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (n Notifications) ComposeInputBulkFormat() string {
+	finalFormat := make([]string, len(n))
+	baseFormat := "('%s',%v,'%s')"
+	for i:=0; i < len(n); i++ {
+		finalFormat[i] = fmt.Sprintf(baseFormat, n[i].UserId, n[i].TopicId, n[i].Message)
+	}
+	return strings.Join(finalFormat, ",")
+}
+
+func (n Notifications) ComposeIdBulkFormat() string {
+	finalFormat := make([]string, len(n))
+	for i:=0; i < len(n); i++ {
+		finalFormat[i] = strconv.Itoa(n[i].Id)
+	}
+	return "(" + strings.Join(finalFormat, ",") + ")"
+}
+
+func (n Notifications) UpdateReadNotification(tx ITransaction) error {
+	path := "notification.updateReadNotification"
+	if err := WriteToDB(tx, path, n.ComposeIdBulkFormat()); err != nil {
+		return err
+	}
+	return nil
+}
--- database/database_test.go
+++ database/database_test.go
@@ -3,7 +3,19 @@ package database
 import (
 	"testing"
 	"database/sql"
+	"reflect"
 )
+
+func TestConvertJsonToQueryMap(t *testing.T) {
+	dir := "queryMap.json"
+	if err := ConvertJsonToQueryMap(dir); err != nil {
+		t.Fatalf("Failed to read query map")
+	}
+	qm := make(QueryMap)
+	if reflect.TypeOf(queryMap) != reflect.TypeOf(qm) {
+		t.Fatalf("Not a query map")
+	}
+}
 
 func TestConnectionDatabase(t *testing.T) {
 	connProp := MysqlDatabaseAccess{
@@ -13,7 +25,7 @@ func TestConnectionDatabase(t *testing.T) {
 		Address: "localhost",
 		DBName: "try1",
 	}
-	connDB, err := ConnectDatabase(connProp)
+	connDB, err := connProp.ConnectDatabase()
 	if err != nil {
 		t.Fatalf("Cannot Connect to DB")
 	}
@@ -25,13 +37,15 @@ func TestConnectionDatabase(t *testing.T) {
 }
 
 func TestGetUser(t *testing.T) {
-	userId := "user/2020/05c82a88/b66e"
-	result, err := GetUser(userId)
+	userProfile := UserProfile{
+		Id: "user/2020/05c82a88/b66e",
+	}
+	err := userProfile.Get(db)
 	if err != nil {
-		
+		t.Fatalf("%v", err)
 	}
 	want := "new@user.com"
-	yield := result.Email
+	yield := userProfile.Email
 	if want != yield {
 		t.Fatalf("want %v yield %v", want, yield)
 	}
@@ -44,16 +58,18 @@ func TestInsertUserEmailAndId(t *testing.T) {
 		Id: userId,
 		Email: email,
 	}
-	if ok := InsertUserEmailAndId(userProfile); !ok {
-		t.Fatalf("Error inserting")
+	if err := userProfile.Insert(db); err != nil {
+		t.Fatalf("Failed to Insert User %v", err)
 	}
 
-	result, err := GetUser(userId)
-	if err != nil {
+	newUserProfile := UserProfile{
+		Id: userId,
+	}
+	if err := newUserProfile.Get(db); err != nil {
 		t.Fatalf("Failed to get user profile %v", err)
 	}
-	if userProfile.Email != result.Email {
-		t.Fatalf("Want %v yield %v", userProfile.Email, result.Email)
+	if userProfile.Email != newUserProfile.Email {
+		t.Fatalf("Want %v yield %v", userProfile.Email, newUserProfile.Email)
 	}	
 }
 
@@ -64,15 +80,18 @@ func TestUpdateUserEmail(t *testing.T) {
 		Email: email,
 		Id: userId,
 	}
-	if ok := UpdateUserEmail(userProfile); !ok {
-		t.Fatalf("Failed to udpate")
+	if err := userProfile.Update(db); err != nil {
+		t.Fatalf("Failed to udpate %v", err)
 	}
-	result, err := GetUser(userId)
+	newUserProfile := UserProfile{
+		Id: userId,
+	}
+	err := newUserProfile.Get(db)
 	if err != nil {
 		t.Fatalf("Failed to get user profile %v", err)
 	}
-	if userProfile.Email != result.Email {
-		t.Fatalf("Want %v yield %v", userProfile.Email, result.Email)
+	if userProfile.Email != newUserProfile.Email {
+		t.Fatalf("Want %v yield %v", userProfile.Email, newUserProfile.Email)
 	}
 }
 
@@ -81,14 +100,123 @@ func TestDeleteUser(t *testing.T) {
 	userProfile := UserProfile{
 		Id: userId,
 	}
-	if ok := DeleteUser(userProfile); !ok {
-		t.Fatalf("Failed to delete")
+	if err := userProfile.Delete(db); err != nil {
+		t.Fatalf("Failed to delete %v", err)
 	}
-	result, err := GetUser(userId)
+	newUserProfile := UserProfile{
+		Id: userId,
+	}
+	err := newUserProfile.Get(db)
 	if err != sql.ErrNoRows {
 		t.Fatalf("Failed to get user profile %v", err)
 	}
-	if result.Id != "" {
+	if newUserProfile.Email != "" {
 		t.Fatalf("Should be empty")
 	} 
+}
+
+func TestBulkFormat(t *testing.T) {
+	insertArray := Notifications{
+		Notification{
+			Id: 23,
+			UserId: "user/1",
+			TopicId: 2,
+			Message: "Hello",
+		},
+		Notification{
+			Id: 24,
+			UserId: "user/2",
+			TopicId: 2,
+			Message: "Hello",
+		},
+	}
+	bulkFormat := insertArray.ComposeInputBulkFormat()
+	want := `('user/1',2,'Hello'),('user/2',2,'Hello')`
+	if want != bulkFormat {
+		t.Fatalf("\nwant %v; \nget  %v", want, bulkFormat)
+	}
+	bulkFormat = insertArray.ComposeIdBulkFormat()
+	want = `(23,24)`
+	if want != bulkFormat {
+		t.Fatalf("\nwant %v; \nget  %v", want, bulkFormat)
+	}
+}
+
+func TestUpdateNotificationIsRead(t *testing.T) {
+	user1 := UserProfile{
+		Id: "user/1",
+		Email: "asdf@adsf.com",
+	}
+	user2 := UserProfile{
+		Id: "user/2",
+		Email: "dddd@gmz.com",
+	}
+	user3 := UserProfile{
+		Id: "user/3",
+		Email: "aaaa@gmz.com",
+	}
+	topic := Topic{
+		UserId: "user/3",
+		Title: "Hello",
+		Desc: "try1",
+	}
+	insertArray := Notifications{
+		Notification{
+			Id: 1,
+			UserId: "user/1",
+			TopicId: 1,
+			Message: "Hello",
+		},
+		Notification{
+			Id: 2,
+			UserId: "user/2",
+			TopicId: 1,
+			Message: "Hello",
+		},
+	}
+	if err := CreateTransaction(func(tx *sql.Tx) error {
+
+		if err := user1.Delete(tx); err != nil {
+			return err
+		}
+		if err := user2.Delete(tx); err != nil {
+			return err
+		}
+		if err := user3.Delete(tx); err != nil {
+			return err
+		}
+		if err := user1.Insert(tx); err != nil {
+			return err
+		}
+		if err := user2.Insert(tx); err != nil {
+			return err
+		}
+		if err := user3.Insert(tx); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	
+	if err := topic.Insert(db); err != nil {
+		t.Fatalf("%v", err)
+	}
+	if err := insertArray.Insert(db); err != nil {
+		t.Fatalf("%v", err)
+	}
+	
+	if err := insertArray.UpdateReadNotification(db); err != nil {
+		t.Fatalf("Failed to update notification")
+	}
+	note := Notification{
+		Id: 1,
+	}
+	if err := note.Get(db); err != nil {
+		t.Fatalf("Failed to get notification %v", err)
+	}
+	if !note.IsRead {
+		t.Fatalf("want %t yield %t", true, note.IsRead)
+	}
 }
